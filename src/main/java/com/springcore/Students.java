4fce759b23812c63d1347955e7153bcb607c25eb
Jitesh Singh <Jitust98@gmail.com>
2021-03-08 11:26:27 +0530

    Spring Core Property Injection By Setter

---
+++ src/main/java/com/springcore/Students.java
@@ -0,0 +1,21 @@
+package com.springcore;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Students {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        
+        StudentBean beanObject = (StudentBean) context.getBean("stuBean");
+        System.out.println(beanObject);
+
+        StudentBean beanObject2 = (StudentBean) context.getBean("stuBean2");
+        System.out.println(beanObject2);
+
+
+    }
+
+}

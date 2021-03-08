06ee19c84e0f62746f4b18d46fd9d64c1fe8d9da
Jitesh Singh <Jitust98@gmail.com>
2021-03-08 11:35:49 +0530

    Spring Core Property Injection(Collection Type) By Setter

--- src/main/java/com/springcore/Students.java
+++ src/main/java/com/springcore/Students.java
@@ -1,5 +1,6 @@
 package com.springcore;
 
+import com.springcore.collection.StudentDetailsBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,13 +9,16 @@ public class Students {
     public static void main(String[] args) {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
-        
+
         StudentBean beanObject = (StudentBean) context.getBean("stuBean");
         System.out.println(beanObject);
 
         StudentBean beanObject2 = (StudentBean) context.getBean("stuBean2");
         System.out.println(beanObject2);
 
+        ApplicationContext detailsBeanContext = new ClassPathXmlApplicationContext("collection_config.xml");
+        StudentDetailsBean detailsBean = (StudentDetailsBean) detailsBeanContext.getBean("StuDetails");
+        System.out.println(detailsBean);
 
     }
 

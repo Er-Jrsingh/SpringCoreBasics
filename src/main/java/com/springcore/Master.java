bd5c0344937a7d63d739c542b5d569c47b742198
Jitesh Singh <Jitust98@gmail.com>
2021-03-27 13:43:19 +0000

    Create Standalone Collection & Change The Type According To Own

--- src/main/java/com/springcore/Master.java
+++ src/main/java/com/springcore/Master.java
@@ -10,19 +10,25 @@ public class Master {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("collection_config.xml");
 
+        System.out.println("-------------------------------------*** First Bean ***----------------------------------------------");
+
         StudentDetailsBean beanObject = (StudentDetailsBean) context.getBean("StuDetails");
         System.out.println(beanObject);
 
         System.out.println("Type Of List : " + beanObject.getPhones().getClass().getName());
         System.out.println("Type Of Set : " + beanObject.getAdresses().getClass().getName());
         System.out.println("Type Of Map : " + beanObject.getCourses().getClass().getName());
-        
-//        StudentDetailsBean beanObject3 = (StudentDetailsBean) context.getBean("StuDetails");
-//        System.out.println(beanObject3);
-//
-//        System.out.println("Type Of List : " + beanObject3.getPhones().getClass().getName());
-//        System.out.println("Type Of Set : " + beanObject3.getAdresses().getClass().getName());
-//        System.out.println("Type Of Map : " + beanObject3.getCourses().getClass().getName());
+
+        System.out.println("----------------------------*** Create 2 Object With Data By Java ***--------------------------------");
+
+        StudentDetailsBean beanObject3 = (StudentDetailsBean) context.getBean("StuDetails");
+        System.out.println(beanObject3);
+
+        System.out.println("Type Of List : " + beanObject3.getPhones().getClass().getName());
+        System.out.println("Type Of Set : " + beanObject3.getAdresses().getClass().getName());
+        System.out.println("Type Of Map : " + beanObject3.getCourses().getClass().getName());
+
+        System.out.println("-------------------------------------*** Second Bean ***----------------------------------------------");
 
         StudentDetailsBean beanObject2 = (StudentDetailsBean) context.getBean("StuDetails2");
         System.out.println(beanObject2);

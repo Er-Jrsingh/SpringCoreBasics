1937a0f2a829c37448a13cad12740fbdc1c1ded5
Jitesh Singh <Jitust98@gmail.com>
2021-03-27 13:02:30 +0000

    Traditional Method To Create 2 Object With Same Data & By Default Type Of Collections

--- src/main/java/com/springcore/Master.java
+++ src/main/java/com/springcore/Master.java
@@ -1,10 +1,36 @@
 package com.springcore;
 
+import com.springcore.collection.StudentDetailsBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Master {
 
     public static void main(String[] args) {
 
-        System.out.println("");
+        ApplicationContext context = new ClassPathXmlApplicationContext("collection_config.xml");
+
+        StudentDetailsBean beanObject = (StudentDetailsBean) context.getBean("StuDetails");
+        System.out.println(beanObject);
+
+        System.out.println("Type Of List : " + beanObject.getPhones().getClass().getName());
+        System.out.println("Type Of Set : " + beanObject.getAdresses().getClass().getName());
+        System.out.println("Type Of Map : " + beanObject.getCourses().getClass().getName());
+        
+//        StudentDetailsBean beanObject3 = (StudentDetailsBean) context.getBean("StuDetails");
+//        System.out.println(beanObject3);
+//
+//        System.out.println("Type Of List : " + beanObject3.getPhones().getClass().getName());
+//        System.out.println("Type Of Set : " + beanObject3.getAdresses().getClass().getName());
+//        System.out.println("Type Of Map : " + beanObject3.getCourses().getClass().getName());
+
+        StudentDetailsBean beanObject2 = (StudentDetailsBean) context.getBean("StuDetails2");
+        System.out.println(beanObject2);
+
+        System.out.println("Type Of List : " + beanObject2.getPhones().getClass().getName());
+        System.out.println("Type Of Set : " + beanObject2.getAdresses().getClass().getName());
+        System.out.println("Type Of Map : " + beanObject2.getCourses().getClass().getName());
+
     }
 
 }
---
+++ src/main/java/com/springcore/collection/StudentDetailsBean.java
@@ -0,0 +1,51 @@
+package com.springcore.collection;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class StudentDetailsBean {
+
+    private String name;
+    private List<String> phones;
+    private Set<String> Adresses;
+    private Map<String, String> courses;
+
+    @Override
+    public String toString() {
+        return "StudentDetailsBean{" + "name=" + name + ", phones=" + phones + ", Adresses=" + Adresses + ", courses=" + courses + '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getPhones() {
+        return phones;
+    }
+
+    public void setPhones(List<String> phones) {
+        this.phones = phones;
+    }
+
+    public Set<String> getAdresses() {
+        return Adresses;
+    }
+
+    public void setAdresses(Set<String> Adresses) {
+        this.Adresses = Adresses;
+    }
+
+    public Map<String, String> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Map<String, String> courses) {
+        this.courses = courses;
+    }
+
+}

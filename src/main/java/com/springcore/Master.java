c5ebf3586952f886b2ef3588bb09147d99a43b0a
Jitesh Singh <Jitust98@gmail.com>
2021-04-24 11:41:00 +0000

    Create Bean Using Custom Java Config Class & @Component Annotation

--- src/main/java/com/springcore/Master.java
+++ src/main/java/com/springcore/Master.java
@@ -1,10 +1,16 @@
 package com.springcore;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Master {
 
     public static void main(String[] args) {
 
-        System.out.println("");
+        ApplicationContext context=new AnnotationConfigApplicationContext(AnnotConfig.class);
+        Student student=context.getBean("student",Student.class);
+        student.study();
+        System.out.println(student);
     }
 
 }

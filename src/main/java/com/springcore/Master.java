71f597468bc393ac4d09bc76d275c04aae69f9a9
Jitesh Singh <Jitust98@gmail.com>
2021-04-26 17:39:29 +0000

    Configure Jdbc Template To Execute Query

--- src/main/java/com/springcore/Master.java
+++ src/main/java/com/springcore/Master.java
@@ -1,10 +1,17 @@
 package com.springcore;
 
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class Master {
 
     public static void main(String[] args) {
 
-        System.out.println("");
-    }
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        JdbcTemplate template = context.getBean("jdbcTemplate", JdbcTemplate.class);
 
+
+    }
 }
